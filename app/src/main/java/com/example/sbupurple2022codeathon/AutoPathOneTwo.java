6df81fa6545ec2c6924f68acd9c35b46db94efed
ldh2001 <s836517@sbuniv.edu>
2022-04-09 15:45:03 -0500

    Created a few steps for tire change

---
+++ app/src/main/java/com/example/sbupurple2022codeathon/AutoPathOneTwo.java
@@ -0,0 +1,47 @@
+package com.example.sbupurple2022codeathon;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.sbupurple2022codeathon.databinding.AutoPathOneTwoBinding;
+
+public class AutoPathOneTwo extends Fragment {
+
+    private AutoPathOneTwoBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = AutoPathOneTwoBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(AutoPathOneTwo.this)
+                        .navigate(R.id.action_LifeHome_to_FirstFragment);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
